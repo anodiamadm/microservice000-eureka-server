cad13a941fd3cab36822ef9bccd98bb94f5d5453
anodiamadm <anodiamadm@gmail.com>
2021-09-08 09:32:41 +1000

    first commit

---
+++ src/main/java/com/anodiam/services/AnodiamEurekaServer/AnodiamEurekaServerApplication.java
@@ -0,0 +1,15 @@
+package com.anodiam.services.AnodiamEurekaServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class AnodiamEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnodiamEurekaServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/anodiam/services/AnodiamEurekaServer/AnodiamEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anodiam.services.AnodiamEurekaServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnodiamEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
